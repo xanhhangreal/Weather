7379813bb955d28a3dc415dfd8b643e4a2ff49d9
Xuan Anh <anhnguyenkaz@gmail.com>
2025-03-25 01:53:48 +0700

    Khởi tạo project Weather

---
+++ app/src/main/java/ie/weather/WeeklyWeatherFragment.java
@@ -0,0 +1,18 @@
+package ie.weather;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeeklyWeatherFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weekly_weather, container, false);
+    }
+}
